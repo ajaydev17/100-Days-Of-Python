6041a7eec532192a6395a7ca25facc23823fd89d
Ajay Dev <devadigaajay1729@gmail.com>
2023-07-15 12:39:46 +0530

    Day 2: string data type example

---
+++ Day 2/string_type.py
@@ -0,0 +1,16 @@
+my_string = "Hello World"
+
+# get the length of the string
+print(len(my_string))
+
+# indexing example
+print(my_string[0])
+
+# negetive indexing will also works and it starts from -1 i.e last character of the string
+print(my_string[-1])
+
+# reverse a string with negetive indexing
+print(my_string[::-1])
+
+# get the type using type function
+print(type(my_string))
