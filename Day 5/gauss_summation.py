5f200f9491f54af266a096156a785215a0a4dde1
Ajay Dev <devadigaajay1729@gmail.com>
2023-07-25 01:16:49 +0530

    Day 5: Gauss summation example

---
+++ Day 5/gauss_summation.py
@@ -0,0 +1,7 @@
+# print some message on the console
+print("Guass summation ie. sum of intgers from 1 to 100")
+
+total_sum = 0
+for number in range(1, 101):
+    total_sum += number
+print(f"The Gauss summation is : {total_sum}")
