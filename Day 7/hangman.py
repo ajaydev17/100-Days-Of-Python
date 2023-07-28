d9f6a39833dec2990cced7f06c90ec82737e964d
Ajay Dev <devadigaajay1729@gmail.com>
2023-07-28 23:58:37 +0530

    Day 7: Hangman game termination logic

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -3,10 +3,81 @@ import random
 # print some message on the console
 print("Welcome to the Hangman game.........................................................\n")
 
+stages = ['''
+ 
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+ 
+''', '''
+ 
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+ 
+''', '''
+ 
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+ 
+''', '''
+ 
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+ 
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+ 
+''', '''
+ 
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+ 
+''', '''
+ 
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+ 
+''']
+
 word_list = ['aardvark', 'baboon', 'camel']
 
 # select a random word from the list
 chosen_word = random.choice(word_list)
+lives_left = 6
 
 display = []
 for char in chosen_word:
@@ -21,8 +92,13 @@ while end_of_game:
     for position in range(len(chosen_word)):
         if user_guess == chosen_word[position]:
             display[position] = user_guess
+    else:
+        lives_left = lives_left - 1
+        if lives_left == 0:
+            end_of_game = False
+            print("You lose!!")
 
-    print(display)
+    print(f"{''.join(display)}")
 
     if '_' not in display:
         end_of_game = False
