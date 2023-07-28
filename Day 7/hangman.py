33adf770b5c2edeec55cc31734a009bec37f6321
Ajay Dev <devadigaajay1729@gmail.com>
2023-07-28 23:37:37 +0530

    Day 7: Hangman game termination logic

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -8,16 +8,22 @@ word_list = ['aardvark', 'baboon', 'camel']
 # select a random word from the list
 chosen_word = random.choice(word_list)
 
-# ask the user to guess a word
-user_guess = input("Guess a letter: ")
-
 display = []
-
 for char in chosen_word:
     display += '_'
 
-for position in range(len(chosen_word)):
-    if user_guess == chosen_word[position]:
-        display[position] = user_guess
+end_of_game = True
 
-print(display)
+while end_of_game:
+
+    user_guess = input("Guess a letter: ")
+
+    for position in range(len(chosen_word)):
+        if user_guess == chosen_word[position]:
+            display[position] = user_guess
+
+    print(display)
+
+    if '_' not in display:
+        end_of_game = False
+        print("You win!!")
