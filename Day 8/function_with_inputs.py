1eed6adc38798e2a807231b3d692d7b0665aa04d
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-01 23:58:28 +0530

    Day 8: function example

---
+++ Day 8/function_with_inputs.py
@@ -0,0 +1,11 @@
+# function defination
+# the words which are inside brackets are parameters
+def greet(first_person, second_person):
+    print(f"Hello {first_person}")
+    print(f"How do you do {second_person}")
+    print("Is not the weather nice today!")
+
+
+# function call
+# the words which are inside brackets are arguments
+greet("Tobi", "Naruto")
