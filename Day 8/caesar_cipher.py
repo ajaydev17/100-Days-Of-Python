a2fd706f6d5f5af76c928c795d6fe0ed41ab886f
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-10 00:50:16 +0530

    Day 8: caesar cipher app

---
+++ Day 8/caesar_cipher.py
@@ -0,0 +1,55 @@
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z']
+
+
+def caesar_cipher(message, shift_amount, cipher_direction):
+    result = ''
+
+    if cipher_direction == 'decode':
+        shift_amount = shift_amount * -1
+
+    for char in message:
+        position = alphabet.index(char)
+        new_position = position + shift_amount
+        result = result + alphabet[new_position]
+
+    print(f"Here is the {cipher_direction}d message: {result}")
+
+
+print(logo)
+
+should_end = False
+
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar_cipher(message=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input(
+        "Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye!!")
