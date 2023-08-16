9514d5cb456f67804eca14f3cc7875acbe3390a6
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-16 23:50:04 +0530

    Day 12: scope in python

---
+++ Day 12/scope_example.py
@@ -0,0 +1,10 @@
+enemies_count = 1
+
+
+def increase_enemy_count():
+    enemies_count = 2
+    print(f"Enemies count is: {enemies_count}")
+
+
+increase_enemy_count()
+print(f"Enemies count is: {enemies_count}")
