2a324d588bbef8142b24431729e8d4ac52d3c2ca
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-16 23:55:00 +0530

    Day 12: scope in python

--- Day 12/scope_example.py
+++ Day 12/scope_example.py
@@ -1,10 +1,19 @@
+# this variable is in global scope
 enemies_count = 1
 
 
 def increase_enemy_count():
+    # this variable is in local scope, changing this localvariable will not effect the global variable enemies_count
+
+    # we can change the global enemies_count with accessing it inside this function with global keyword
+    # global enemies_count
+
     enemies_count = 2
+    # printing the local variable enemies_count
     print(f"Enemies count is: {enemies_count}")
 
 
 increase_enemy_count()
+
+# this gonna print the global enemies_count variable
 print(f"Enemies count is: {enemies_count}")
