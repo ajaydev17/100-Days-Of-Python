89d43a6fe569cb2350cb35330bc62e2de966475f
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-17 00:11:24 +0530

    Day 12: scope in python

--- Day 12/scope_example.py
+++ Day 12/scope_example.py
@@ -8,6 +8,8 @@ def increase_enemy_count():
     # we can change the global enemies_count with accessing it inside this function with global keyword
     # global enemies_count
 
+    # but above step is not preferred one because global variables should not be changed inside a local scope
+
     enemies_count = 2
     # printing the local variable enemies_count
     print(f"Enemies count is: {enemies_count}")
