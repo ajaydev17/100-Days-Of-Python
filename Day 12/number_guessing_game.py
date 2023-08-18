b9d2b62e89e9f8fe5517b5b3ab7d2ea6db24976c
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-18 23:34:51 +0530

    Day 12: Number guessing game part 2

--- Day 12/number_guessing_game.py
+++ Day 12/number_guessing_game.py
@@ -26,14 +26,40 @@ def check_answer(guess, answer, turns):
         print("Too low!!")
         return turns - 1
     else:
-        print(f"You got it right!!, the answer was {answer}")
+        print(f"You got it right!!, the answer was {answer}!!")
 
 # sets the difficulty of the game.
 
 
 def set_difficulty():
-    choice = input("hoose your difficulty. Type 'easy' or 'hard': ")
+    choice = input("Choose your difficulty. Type 'easy' or 'hard': ")
     if choice == 'easy':
         return EASY_LEVEL_TURNS
     else:
         return HARD_LEVEL_TURNS
+
+
+def game():
+    print(logo)
+
+    print("Welcome to the number guessing game!!!")
+    print("I'm thinking of a number between 1 and 100.")
+    answer = randint(1, 100)
+    print(f"Pssst, the correct answer is {answer}")
+
+    turns = set_difficulty()
+    guess = 0
+
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, answer, turns)
+
+        if turns == 0:
+            print("You've run out of guesses, you lose.")
+            return
+        elif guess != answer:
+            print("Guess again!!")
+
+
+game()
