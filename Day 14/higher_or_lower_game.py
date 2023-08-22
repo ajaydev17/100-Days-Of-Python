fce1458942b00607b65c77c5324ef990cdc7c424
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-23 00:18:53 +0530

    Day 14: higher or lower game final changes

--- Day 14/higher_or_lower_game.py
+++ Day 14/higher_or_lower_game.py
@@ -1,6 +1,8 @@
 # display the art
 
 import random
+import os
+
 logo = """
     __  ___       __             
    / / / (_)___ _/ /_  ___  _____
@@ -359,3 +361,42 @@ def check_answer(guess, a_followers, b_followers):
         return guess == 'a'
     else:
         return guess == 'b'
+
+
+def game():
+    print(logo)
+    score = 0
+    game_should_continue = True
+
+    account_a = get_random_data()
+    account_b = get_random_data()
+
+    while game_should_continue:
+        account_a = account_b
+        account_b = get_random_data()
+
+        while account_a == account_b:
+            account_b = get_random_data()
+
+        print(f"Compare A: {get_formatted_data(account_a)}")
+        print(vs)
+        print(f"Compare B: {get_formatted_data(account_b)}")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_followers_count = account_a["follower_count"]
+        b_followers_count = account_b["follower_count"]
+
+        result = check_answer(guess, a_followers_count, b_followers_count)
+
+        os.system('cls')
+        print(logo)
+
+        if result:
+            score = score + 1
+            print(f"You are right, current score is: {score}")
+        else:
+            game_should_continue = False
+            print(f"Sorry!!, that is wrong. final score is: {score}")
+
+
+game()
