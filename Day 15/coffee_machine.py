0c4f6d388be9fc9838a25cbd14c795348ee9dc37
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-24 00:34:46 +0530

    Day 15: coffee machine initial commit

--- Day 15/coffee_machine.py
+++ Day 15/coffee_machine.py
@@ -24,8 +24,41 @@ MENU = {
     }
 }
 
+profit = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
+
+
+def is_resource_sufficient(order_ingredients):
+    """
+        gonna return whether we can prefer the drink or not with the resource available
+    """
+    is_enough = True
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry!!, There is not enough {item} make your drink.")
+            is_enough = False
+    return is_enough
+
+
+is_machine_on = True
+
+while is_machine_on:
+    choice = input("What would you like to have? (espresso/latte/cappuccino): ").lower()
+    if choice == "off":
+        is_machine_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        if choice not in MENU:
+            print("Not a valid choice!!, please select one of the drinks from the option above")
+        else:
+            drink_selected = MENU[choice]
+            if is_resource_sufficient(drink_selected["ingredients"]):
+                print(f"We can prepare the {choice} for you, wait for sometime.")
