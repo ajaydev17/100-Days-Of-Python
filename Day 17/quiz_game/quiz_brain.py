ba1da2e0099e1a745b04e309f96d083c32e2d281
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-30 01:38:09 +0530

    Day 17: Quiz app creating the question bank

--- Day 17/quiz_game/quiz_brain.py
+++
@@ -1,6 +0,0 @@
-class Question:
-
-    # constructor function definition
-    def __init__(self, question_text, question_answer):
-        self.question_text = question_text
-        self.question_answer = question_answer
