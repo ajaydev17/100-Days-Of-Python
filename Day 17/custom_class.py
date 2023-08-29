8d4d0d76ab138beb0fdecdf8a112a46a35cbf029
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-30 00:11:13 +0530

    Day 17: custom class example

--- Day 17/custom_class.py
+++ Day 17/custom_class.py
@@ -2,7 +2,18 @@
 # we can create an empty class with pass keywords
 
 class User:
-    pass
+
+    # defining the constructor function
+    def __init__(self, user_id, user_name):
+        self.user_id = user_id
+        self.user_name = user_name
+        self.followers = 0
+        self.following = 0
+
+    # class based functions
+    def follow(self, user_prof):
+        self.following += 1
+        user_prof.followers += 1
 
 
 def my_function():
@@ -12,5 +23,13 @@ def my_function():
     pass
 
 
-user_1 = User()
-print(user_1)
+user_1 = User("001", "John")
+user_2 = User("002", "Smith")
+
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
+
+print(user_2.followers)
+print(user_2.following)
