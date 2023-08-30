2ca90b166bfd8885292be85d79437f64b01fc293
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-31 01:39:01 +0530

    Day 17: Quiz app generating the next question

--- Day 17/quiz_game/main.py
+++ Day 17/quiz_game/main.py
@@ -12,4 +12,6 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_question():
+    quiz.next_question()
--- Day 17/quiz_game/quiz_brain.py
+++ Day 17/quiz_game/quiz_brain.py
@@ -5,11 +5,16 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = q_list
 
+    def still_has_question(self):
+        """
+            returns whether question list is empty or not
+        """
+        return self.question_number < len(self.question_list)
+
     def next_question(self):
         """
             returns the next question from the list
         """
         current_question = self.question_list[self.question_number]
-        print(current_question)
         self.question_number += 1
         input(f"Q.{self.question_number}: {current_question.question_text} (True/False)?: ")
