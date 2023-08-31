320bde5b37da52ec042e6d5d883c9905f9083f41
Ajay Dev <devadigaajay1729@gmail.com>
2023-08-31 23:32:00 +0530

    Day 17: Quiz app final changes

--- Day 17/quiz_game/quiz_brain.py
+++ Day 17/quiz_game/quiz_brain.py
@@ -4,6 +4,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
+        self.score = 0
 
     def still_has_question(self):
         """
@@ -17,4 +18,18 @@ class QuizBrain:
         """
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.question_text} (True/False)?: ")
+        user_guess = input(f"Q.{self.question_number}: {current_question.question_text} (True/False)?: ")
+        self.check_answer(current_question.question_answer, user_guess)
+
+    def check_answer(self, actual_answer, user_answer):
+        """
+            checks the user answer with the actual answer
+        """
+        if actual_answer.lower() == user_answer.lower():
+            self.score += 1
+            print("You got it right!!")
+        else:
+            print("That's wrong!!")
+        print(f"The correct answer was: {actual_answer}")
+        print(f"The current score is: {self.score}/{self.question_number}")
+        print("\n")
