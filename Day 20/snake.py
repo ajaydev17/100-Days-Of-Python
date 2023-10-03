665bec65f3221c3abf69f87da02ef56bc3e4435c
Ajay Dev <devadigaajay1729@gmail.com>
2023-10-03 16:24:55 +0530

    Day 20: Snake game with OOPS.

---
+++ Day 20/snake.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+SNAKE_POSITION = [(0, 0), [-20, 0], [-40, 0]]
+MOVE_DISTANCE = 20
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in SNAKE_POSITION:
+            new_turtle = Turtle(shape="square")
+            new_turtle.color("white")
+            new_turtle.penup()
+            new_turtle.goto(position)
+            self.segments.append(new_turtle)
+
+    def move(self):
+        for index in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[index - 1].xcor()
+            new_y = self.segments[index - 1].ycor()
+            self.segments[index].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+        # self.segments[0].left(90)
