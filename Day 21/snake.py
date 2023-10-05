a1a71979b343a5e582c890f44402538b87bcaf41
Ajay Dev <devadigaajay1729@gmail.com>
2023-10-06 00:04:38 +0530

    Day 20: Snake game detect colliosion with food.

---
+++ Day 21/snake.py
@@ -0,0 +1,49 @@
+from turtle import Turtle
+
+SNAKE_POSITION = [(0, 0), [-20, 0], [-40, 0]]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in SNAKE_POSITION:
+            new_turtle = Turtle(shape="square")
+            new_turtle.shapesize(stretch_wid=1, stretch_len=1)
+            new_turtle.color("white")
+            new_turtle.penup()
+            new_turtle.goto(position)
+            self.segments.append(new_turtle)
+
+    def move(self):
+        for index in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[index - 1].xcor()
+            new_y = self.segments[index - 1].ycor()
+            self.segments[index].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+        # self.segments[0].left(90)
+
+    def move_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def move_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def move_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def move_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
