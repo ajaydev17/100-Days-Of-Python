2f5e7781a7bbc5a05d60511895657fe9f36846a5
Ajay Dev <devadigaajay1729@gmail.com>
2023-10-07 18:06:45 +0530

    Day 20: Snake game updating the score board.

--- Day 21/main.py
+++ Day 21/main.py
@@ -2,6 +2,7 @@ from turtle import Turtle, Screen
 import time
 from snake import Snake
 from food import Food
+from scoreboard import ScoreBoard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+score_board = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.move_up, "Up")
@@ -28,6 +30,7 @@ while game_is_on:
 
     if snake.head.distance(food) < 15:
         food.refresh()
+        score_board.increase_score()
 
 
 screen.exitonclick()
