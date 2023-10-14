42ab62510af273daf6249e5c3558175da2746978
Ajay Dev <devadigaajay1729@gmail.com>
2023-10-14 22:13:22 +0530

    Creating the ball

---
+++ Day 22/ball.py
@@ -0,0 +1,15 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+
+    def move(self):
+        new_xcor = self.xcor() + 10
+        new_ycor = self.ycor() + 10
+        self.goto(new_xcor, new_ycor)
