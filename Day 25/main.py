86d07ff7de9456186cab2771004be3364041f9cf
Ajay Dev <devadigaajay1729@gmail.com>
2023-10-26 01:36:50 +0530

    Day 25: pandas with csv

--- Day 25/main.py
+++ Day 25/main.py
@@ -25,3 +25,35 @@ print(data)
 
 # get the temperature
 print(data["temp"])
+
+# convert dataframe to dict
+dict_data = data.to_dict()
+print(dict_data)
+
+# convert series to list
+list_data = data["temp"].to_list()
+print(list_data)
+
+# get the mean and maximum values
+print(data["temp"].mean())
+print(data["temp"].max())
+
+# different ways to get the data from column
+print(data["condition"])
+print(data.condition)
+
+# get the data from row based on condition
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()])
+
+# create dataframe from scratch
+student_scores = {
+    "names": ["James", "John", "Speed"],
+    "scores": [89, 85, 95]
+}
+
+data = pd.DataFrame(student_scores)
+print(data)
+
+# save the data as csv
+data.to_csv("student_scores.csv")
