e6dc36d7e8c188b3b8b7d56c1f1e15314d7fb86b
Ajay Dev <devadigaajay1729@gmail.com>
2023-11-25 01:41:47 +0530

    Day 31: Creating the UI.

--- Day 31/main.py
+++ Day 31/main.py
@@ -1,2 +1,31 @@
+from tkinter import Tk, Canvas, PhotoImage, Button
+
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+
+# creating the window
+window = Tk()
+window.title("Flashy Cards")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+
+canvas = Canvas(width=800, height=526)
+card_front_image = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_image)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.create_text(400, 150, text="Title", font=('Ariel', 40, 'italic'))
+canvas.create_text(400, 263, text="Word", font=('Ariel', 60, 'bold'))
+canvas.grid(row=0, column=0, columnspan=2)
+
+
+cross_image = PhotoImage(file="images/wrong.png")
+cross_button = Button(image=cross_image, highlightthickness=0)
+cross_button.grid(row=1, column=0)
+
+right_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_image, highlightthickness=0)
+right_button.grid(row=1, column=1)
+
+window.mainloop()
 
